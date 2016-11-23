20f52775f05afed62535d7be09ed4a38cc0b0e18
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-11-23 16:19:02 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+User.create(first_name: "Tito", last_name: "Mueller")
+
+RecipeTitle.delete_all
+RecipeTitle.create(name: "Chili con Carne", category: "Fleisch", ingredient: "Rindfleisch, Bohnen")
+RecipeTitle.create(name: "Wrap", category: "Gemuese", ingredient: "Mozzarella, Salat, Pfannkuchen")
